4ccf25c48a0f7addff755e44619059bd6483a084
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2023-01-18 17:57:09 +0100

    Test express and dotenv - OK

---
+++ src/server/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  process.stdout.write(`Server started on port: ${port}\n`);
+});
+
+app.use('/', (request, response) => {
+  response.send("Hello World !");
+});
