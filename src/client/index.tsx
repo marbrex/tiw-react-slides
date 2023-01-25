3017c59b581acadc0999048f19d5b313d0e2efbe
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2023-01-25 17:48:58 +0100

    Add an Error Route for Client

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import ErrorPage from './routes/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />
   }
 ])
 
