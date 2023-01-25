16ecd24c291ed6183e95ea1d5f06bb406810dcde
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2023-01-25 17:48:16 +0100

    Make server to use the Parcel bundle

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,6 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import express from 'express'
 import * as dotenv from 'dotenv'
 
@@ -5,10 +8,16 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  process.stdout.write(`Server started on port: ${port}\n`)
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+const DIST_DIR = path.join(dirname, '../../dist')
+app.use(express.static(DIST_DIR))
+
+app.all('*', (req, res) => {
+  res.sendFile(path.join(DIST_DIR, '/index.html'))
 })
 
-app.use('/', (request, response) => {
-  response.send('Hello World !')
+app.listen(port, () => {
+  process.stdout.write(`Server started on port: ${port}\n`)
 })
