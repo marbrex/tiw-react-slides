16c52bd56d1dfd440ab2557a91dc54da03517ebc
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2023-01-28 20:20:55 +0100

    Add Store Model, Hooks

---
+++ src/client/application/store/index.ts
@@ -0,0 +1,30 @@
+import { action, computed, createStore } from 'easy-peasy'
+import SLIDES from '../../slides'
+import { SlideStoreModel } from './model'
+
+const storeModel: SlideStoreModel = {
+  name: 'slidesApp',
+  slides: SLIDES,
+  slideIndex: 0,
+  slidesCount: computed(state => state.slides.length),
+  isVisible: computed(state => state.slides[state.slideIndex].visible),
+  slide: computed(state => state.slides[state.slideIndex]),
+  nextSlide: action(state => {
+    state.slideIndex = state.slideIndex < state.slides.length - 1 ? state.slideIndex + 1 : state.slideIndex
+  }),
+  previousSlide: action(state => {
+    state.slideIndex = state.slideIndex > 0 ? state.slideIndex - 1 : state.slideIndex
+  }),
+  setSlideIndex: action((state, payload) => {
+    state.slideIndex = payload.slideIndex
+  }),
+  toggleSlideVisibility: action(state => {
+    state.slides[state.slideIndex].visible = !state.slides[state.slideIndex].visible
+  })
+}
+
+const store = createStore(storeModel, {
+  middleware: []
+})
+
+export default store
---
+++ src/client/application/store/model.ts
@@ -0,0 +1,18 @@
+import { Action, Computed } from 'easy-peasy'
+
+export interface SlideStoreModel {
+  name: string
+  slides: Slide[]
+  slideIndex: number
+  slidesCount: Computed<SlideStoreModel, number>
+  slide: Computed<SlideStoreModel, Slide>
+  isVisible: Computed<SlideStoreModel, boolean>
+  nextSlide: Action<SlideStoreModel>
+  previousSlide: Action<SlideStoreModel>
+  setSlideIndex: Action<SlideStoreModel, SetSlideAction>
+  toggleSlideVisibility: Action<SlideStoreModel>
+}
+
+export interface SetSlideAction {
+  slideIndex: number
+}
